b32fc714c63da263ca57244b5d1636d126587938
David Flores <dehbid12@gmail.com>
2012-08-22 16:29:17 -0700

    update

--- assignments/Aziz_Assign1_Salary.cpp
+++ assignments/Aziz_Assign1_Salary.cpp
@@ -11,33 +11,37 @@ using namespace std;
 
 int main()
 {
-	//Variables
-	int	   id;
-	double hours;
-	double rate;
-	double salary;
+    //Variables
+    int	   id;
+    double hours;
+    double rate;
+    double salary;
 
-	//Title
-	cout << "Author's Name: David Flores             "  << endl;
-	cout << "Program Name : Aziz_Assign1_Salary.cpp  "  << endl;
-	cout << "Date         : August 22, 2012          "  << endl;
-	cout << "School Name  : Long Beach City College  "  << endl;
-	cout << "======================================\n"  << endl;
-	
+    //Title
+    cout << "Author's Name: David Flores             "  << endl;
+    cout << "Program Name : Aziz_Assign1_Salary.cpp  "  << endl;
+    cout << "Date         : August 22, 2012          "  << endl;
+    cout << "School Name  : Long Beach City College  "  << endl;
+    cout << "======================================\n"  << endl;
+    cout << "Employee Salary Records"    << endl;
+    cout << "======================="    << endl;
+    
 	//User Input
-	cout << "Please Enter your Employee ID: ";
-	cin  >> id;
-	cout << "Please Enter Hours Worked:     ";
-	cin  >> hours;
-	cout << "Please Enter Rate Per Hour:    ";
-	cin  >> rate;
+    cout << "Please Enter your Employee ID: ";
+    cin  >> id;
+    cout << "Please Enter Hours Worked:     ";
+    cin  >> hours;
+    cout << "Please Enter Rate Per Hour:    ";
+    cin  >> rate;
 
-	salary = hours * rate;
+    salary = hours * rate;
 
-	//Output
-	cout << "======================================\n"  << endl;
-	cout << "Employee ID  : "  << id     << endl;
-	cout << "Hours Worked : "  << hours  << endl;
-	cout << "Rate per Hour: $" << rate   << endl;
-	cout << "Gross Salary : $" << salary << "/week" << endl;
+    //Output (Result of User Input)
+    cout << "\n"						 << endl;
+    cout << "Employee ID  : "  << id     << endl;
+    cout << "Hours Worked : "  << hours  << endl;
+    cout << "Rate per Hour: $" << rate   << "/hour" << endl;
+    cout << "Gross Salary : $" << salary << "/week" << endl;
+
+    return 0;
 }
