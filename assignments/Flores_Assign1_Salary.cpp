c24d125c956be33a976121d2a18c2b95ab252318
David Flores <dehbid12@gmail.com>
2012-09-13 15:23:19 -0700

    commit

--- assignments/Flores_Assign1_Salary.cpp
+++ assignments/Flores_Assign1_Salary.cpp
@@ -1,10 +1,10 @@
 /*		Author: David Flores
-		Date: Aug. 22, 2012
-		Program: Aziz_Assign1_Salary.cpp
-		Description: This program displays
-		EmployeeID, Hours worked, Rate per
-		hour, and Gross Salary
-*/
+ *		Date: Aug. 22, 2012
+ *		Program: Aziz_Assign1_Salary.cpp
+ *		Description: This program displays
+ *		EmployeeID, Hours worked, Rate per
+ *		hour, and Gross Salary
+ */
 
 #include <iostream>
 using namespace std;
