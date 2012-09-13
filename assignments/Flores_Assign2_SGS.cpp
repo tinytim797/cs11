3d2cf035843ba58e0d59c1a3f7172f6bf9cc53ef
David Flores <dehbid12@gmail.com>
2012-09-13 15:19:25 -0700

    commit

---
+++ assignments/Flores_Assign2_SGS.cpp
@@ -0,0 +1,71 @@
+/*		Author: David Flores
+ *		Date: Sep 02, 2012
+ *		Program: Aziz_Assign2_SGS.cpp
+ *		Description: Displays grades
+ *		appropriate for the student
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "============================" << endl;
+	cout << "Student Grading System (SGS)" << endl;
+	cout << "Long Beach City College"      << endl;
+	cout << "David Flores"                 << endl;
+	cout << "August 28, 2012"              << endl;
+	cout << "============================" << endl;
+	cout << endl;
+
+	int    id;
+	string name,lname;
+	double gradeOne,gradeTwo,gradeThree;
+
+	cout << "Enter Student ID:     ";
+	cin  >> id;
+	cout << "Enter First Name:     ";
+	cin  >> name;
+	cout << "Enter Last Name:      ";
+	cin  >> lname;
+	cout << "Enter Grade for CS11: ";
+	cin  >> gradeOne;
+	cout << "Enter Grade for CS12: ";
+	cin  >> gradeTwo;
+	cout << "Enter Grade for CS21: ";
+	cin  >> gradeThree;
+	cout << endl;
+
+	double total = ( gradeOne + gradeTwo + gradeThree );
+	double average = ( total / 3 );
+
+	string letterGrade;
+
+	if ( average >= 90 )
+		letterGrade = "A";
+	else if ( average >= 80 )
+		letterGrade = "B";
+	else if ( average >= 70 )
+		letterGrade = "C";
+	else if ( average >= 60 )
+		letterGrade = "D";
+	else
+		letterGrade = "F";
+
+	cout << "Student Information:" << endl;
+	cout << "============================" << endl;
+	cout << "ID: .......... " << id << endl;
+	cout << "First Name: .. " << name << endl;
+	cout << "Last Name: ... " << lname << endl;
+	cout << "Total: ....... " << total << endl;
+	cout << "Average: ..... " << average << "%" << endl;
+	cout << "Grade: ....... " << letterGrade << endl;
+
+	if ( letterGrade == "A" )
+		cout << "Comment: ..... Congratulations on your achievements!" << endl;
+	else if ( letterGrade == "F" )
+		cout << "Comment: ..... Apply Yourself!" << endl;
+
+	return 0;
+}
---
+++ assignments/Flores_Assign3_Paycheck.cpp
@@ -0,0 +1,59 @@
+/*		Author: David Flores
+ *		Date: Sep 10, 2012
+ *		Program: Aziz_Assign3_Paycheck.cpp
+ *		Description: Calculates paycheck
+ *      using tax rate.
+ */
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	//ofstream outFile("output.txt");
+	ofstream outFile;
+
+	const double FedTax = 0.15, StateTax = 0.035, SocSecTax = 0.0575, MedTax = 0.0275, PenPlan = 0.05, HealthIns = 75.0;
+	double fed = 0.0, state = 0.0, socSec = 0.0, med = 0.0, pen = 0.0, gPay = 0.0, netPay = 0.0;
+	string emplFirstName = "";
+	string emplLastName = "";
+
+	//First Name, Last Name & Gross Salary User Input
+	cout << "Paycheck Calculation By David Flores" << endl << endl;
+	cout << "Enter First Name:   ";
+	cin >> emplFirstName;
+	cout << "Enter Last Name:    ";
+	cin >> emplLastName;
+	cout << "Enter Gross Salary: ";
+	cin >> gPay;
+
+	//Salary Deduction Calculations
+	fed = ( FedTax * gPay );
+	state = ( StateTax * gPay );
+	socSec = ( SocSecTax * gPay );
+	med = ( MedTax * gPay );
+	pen = ( PenPlan * gPay );
+	netPay = ( gPay - (fed + state + socSec + med + pen + HealthIns) );
+	
+	//Output to output.txt
+	outFile.open("output.txt");
+
+	outFile << fixed << showpoint << setprecision(2);
+
+	outFile << emplFirstName << " " << emplLastName << endl;
+	outFile << left << setw(30) << setfill('.') << "Gross Salary: "          << " $ " << right << setw(7) << setfill(' ') << gPay      << endl;
+	outFile << left << setw(30) << setfill('.') << "Federal Tax: "           << " $ " << right << setw(7) << setfill(' ') << fed       << endl;
+	outFile << left << setw(30) << setfill('.') << "State Tax: "             << " $ " << right << setw(7) << setfill(' ') << state     << endl;
+	outFile << left << setw(30) << setfill('.') << "Social Security Tax: "   << " $ " << right << setw(7) << setfill(' ') << socSec    << endl;
+	outFile << left << setw(30) << setfill('.') << "Medicare/Medicaid Tax: " << " $ " << right << setw(7) << setfill(' ') << med       << endl;
+	outFile << left << setw(30) << setfill('.') << "Pension Plan: "          << " $ " << right << setw(7) << setfill(' ') << pen       << endl;
+	outFile << left << setw(30) << setfill('.') << "Health Insurance: "      << " $ " << right << setw(7) << setfill(' ') << HealthIns << endl;
+	outFile << left << setw(30) << setfill('.') << "Net Pay: "               << " $ " << right << setw(7) << setfill(' ') << netPay    << endl;
+
+	outFile.close();
+	return 0;
+}
